97b5cc638beb8b960fb5a2fd7270b4ea48d564dc
Geert Geurts <geert@verweggistan.eu>
2026-09-12 20:57:04 +0200

    Scope: modules|Reworked kmod
    kmod: Add CI render configuration file
    
    kmod: add build section with run_exports
    
    kmod: remove unneeded fish from host req.

--- kmod/kmod/build.sh
+++ kmod/kmod/build.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-./autogen.sh c
-./configure --prefix=$PREFIX          \
-            --sysconfdir=$PREFIX/etc      \
-            --libdir=$PREFIX/lib    \
-            --disable-manpages    \
-            --with-bashcompletiondir=$PREFIX/etc/bash_completion.d/ \
-            --with-openssl         \
-            --with-xz              \
-            --with-zstd            \
-            --with-zlib
-make
-make install
-cd $PREFIX
-mkdir sbin
-for target in depmod insmod modinfo modprobe rmmod; do
-  ln -sfv bin/kmod sbin/$target
-done
+# ./autogen.sh c
+# ./configure --prefix=$PREFIX          \
+#             --sysconfdir=$PREFIX/etc      \
+#             --libdir=$PREFIX/lib    \
+#             --disable-manpages    \
+#             --with-bashcompletiondir=$PREFIX/etc/bash_completion.d/ \
+#             --with-openssl         \
+#             --with-xz              \
+#             --with-zstd            \
+#             --with-zlib
+# make
+# make install
+meson setup build \
+    --sbindir="$PREFIX/bin" \
+    --prefix="$PREFIX" \
+    --sysconfdir="$PREFIX/etc" \
+    --libdir="$PREFIX/lib" \
+    --buildtype=release \
+    -Dmanpages=false \
+    -Dbashcompletiondir="$PREFIX/etc/bash_completion.d" \
+    -Dopenssl=enabled \
+    -Dxz=enabled \
+    -Dzstd=enabled \
+    -Dzlib=enabled
 
-ln -sfv bin/kmod bin/lsmod
+meson compile -C build -j "${CPU_COUNT:-2}"
+meson install -C build
